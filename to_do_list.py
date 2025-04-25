2ead809cc36403878c7a1d07c50ee711e27c610a
Mahdirz <mahdirazmkhah45@gmail.com>
2025-04-25 17:38:16 +0330

    chore: add __str__ to Task

--- to_do_list.py
+++ to_do_list.py
@@ -4,6 +4,9 @@ class Task:
         self.describtion = describtion
         self.pripriority = priority
 
+    def __str__(self):
+        return f"{self.title} | {self.describtion} | {self.pripriority}"
+
 
 class ToDoList:
     def __init__(self):
