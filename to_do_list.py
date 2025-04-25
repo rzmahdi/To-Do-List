a3cfa58616f28495632ed7f123cfab8c40fe21f3
Mahdirz <mahdirazmkhah45@gmail.com>
2025-04-25 18:52:59 +0330

    fix: Task property miswriting

--- to_do_list.py
+++ to_do_list.py
@@ -4,10 +4,10 @@ class Task:
     def __init__(self, title, describtion, priority):
         self.title = title
         self.describtion = describtion
-        self.pripriority = priority
+        self.priority = priority
 
     def __str__(self):
-        return f"{self.title} | {self.describtion} | {self.pripriority}"
+        return f"{self.title} | {self.describtion} | {self.priority}"
 
 
 class ToDoList:
